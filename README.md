898332002bbed60e31466910e0fe110cbe20871e
Muhammad Mominul Huque <mominul2082@gmail.com>
2021-06-07 01:56:44 +0600

    Add code and documentations!

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # emojicon
-Convert emoticons B-) into Unicode emoji's 😎
+Convert emoticons `B-)` into Unicode emoji's 😎
+
+```rust
+use emojicon::Emojicon;
+
+fn main() {
+    let emojicon = Emojicon::new();
+    assert_eq!(emojicon.get("B-)"), Some("😎"));
+}
+```
+
+## Acknowledgment
+* [emoticon](https://github.com/wooorm/emoticon) by Titus Wormer
---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+//! Convert emoticons `B-)` into Unicode emoji's 😎
+//!
+//! ```
+//! use emojicon::Emojicon;
+//! # fn main() {
+//! 
+//! let emojicon = Emojicon::new();
+//! assert_eq!(emojicon.get("B-)"), Some("😎"));
+//! # }
+//! ```
+//!
+//! # Acknowledgment
+//! * [emoticon](https://github.com/wooorm/emoticon) by Titus Wormer
+
+use std::collections::HashMap;
+
+mod emoticons;
+
+pub struct Emojicon {
+    emoticons: HashMap<&'static str, &'static str>,
+}
+
+impl Emojicon {
+    /// Creates a new instance of `Emojicon`.
+    pub fn new() -> Self {
+        Self {
+            emoticons: emoticons::emoticons()
+        }
+    }
+
+    /// Gets the emoji for given `term` wrapped in a `Option`.
+    /// 
+    /// ```
+    /// # use emojicon::Emojicon;
+    /// # fn main() {
+    /// let emojicon = Emojicon::new();
+    /// assert_eq!(emojicon.get(":-\"]"), Some("😊"));
+    /// # }
+    /// ```
+    pub fn get(&self, term: &str) -> Option<&str> {
+        self.emoticons.get(term).map(|i| *i)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Emojicon;
+
+    #[test]
+    fn test() {
+        let emoji = Emojicon::new();
+        assert_eq!(emoji.get(":)").unwrap(), "😃");
+        assert_eq!(emoji.get(":Z").unwrap(), "😒");
+        assert_eq!(emoji.get(",:-[").unwrap(), "😓");
+        assert_eq!(emoji.get("smile"), None);
+    }
+}

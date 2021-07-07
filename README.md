c4ba94d6e1373a9a899f56b1ff4b7fe267e7a8ed
Muhammad Mominul Huque <mominul2082@gmail.com>
2021-07-08 03:03:25 +0600

    Search by Github's emoji names support! :tada: Add gemoji as a submodule Emoji data generation support

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # emojicon
-Convert emoticons `B-)` into Unicode emoji's 😎
+Find Emoji by using Emoticons and GitHub's emoji names.
 
 ```rust
 use emojicon::Emojicon;
 
 fn main() {
     let emojicon = Emojicon::new();
-    assert_eq!(emojicon.get("B-)"), Some("😎"));
+    assert_eq!(emojicon.get_by_emoticon("B-)"), Some("😎"));
+    assert_eq!(emojicon.get_by_name("cool").unwrap().collect::<Vec<_>>(), ["😎", "🆒"]);
 }
 ```
 
 ## Acknowledgment
 * [emoticon](https://github.com/wooorm/emoticon) by Titus Wormer
+* [gemoji](https://github.com/github/gemoji) by Github
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,35 @@
-//! Convert emoticons `B-)` into Unicode emoji's 😎
+//! Find Emoji by using Emoticons and GitHub's emoji names.
 //!
 //! ```
 //! use emojicon::Emojicon;
 //! # fn main() {
 //! 
 //! let emojicon = Emojicon::new();
-//! assert_eq!(emojicon.get("B-)"), Some("😎"));
+//! assert_eq!(emojicon.get_by_emoticon("B-)"), Some("😎"));
+//! assert_eq!(emojicon.get_by_name("cool").unwrap().collect::<Vec<_>>(), ["😎", "🆒"]);
 //! # }
 //! ```
 //!
 //! # Acknowledgment
 //! * [emoticon](https://github.com/wooorm/emoticon) by Titus Wormer
+//! * [gemoji](https://github.com/github/gemoji) by Github
 
 use std::collections::HashMap;
 
 mod emoticons;
+mod emojis;
 
 pub struct Emojicon {
     emoticons: HashMap<&'static str, &'static str>,
+    emojis: HashMap<&'static str, Vec<&'static str>>,
 }
 
 impl Emojicon {
     /// Creates a new instance of `Emojicon`.
     pub fn new() -> Self {
         Self {
-            emoticons: emoticons::emoticons()
+            emoticons: emoticons::emoticons(),
+            emojis: emojis::emojis()
         }
     }
 
@@ -34,11 +39,24 @@ impl Emojicon {
     /// # use emojicon::Emojicon;
     /// # fn main() {
     /// let emojicon = Emojicon::new();
-    /// assert_eq!(emojicon.get(":-\"]"), Some("😊"));
+    /// assert_eq!(emojicon.get_by_emoticon(":-\"]"), Some("😊"));
     /// # }
     /// ```
-    pub fn get(&self, emoticon: &str) -> Option<&str> {
+    pub fn get_by_emoticon(&self, emoticon: &str) -> Option<&str> {
         self.emoticons.get(emoticon).map(|i| *i)
+    }
+
+    /// Get emojis by given `name`.
+    ///
+    /// ```
+    /// # use emojicon::Emojicon;
+    /// # fn main() {
+    /// let emojicon = Emojicon::new();
+    /// assert_eq!(emojicon.get_by_name("smile").unwrap().collect::<Vec<_>>(), ["😀", "😄"]);
+    /// # }
+    /// ```
+    pub fn get_by_name(&self, name: &str) -> Option<impl Iterator<Item = &str>> {
+        self.emojis.get(name).map(|v| v.iter().map(|s| *s))
     }
 }
 
@@ -47,11 +65,18 @@ mod tests {
     use super::Emojicon;
 
     #[test]
-    fn test() {
+    fn test_by_emoticon() {
         let emoji = Emojicon::new();
-        assert_eq!(emoji.get(":)").unwrap(), "😃");
-        assert_eq!(emoji.get(":Z").unwrap(), "😒");
-        assert_eq!(emoji.get(",:-[").unwrap(), "😓");
-        assert_eq!(emoji.get("smile"), None);
+        assert_eq!(emoji.get_by_emoticon(":)").unwrap(), "😃");
+        assert_eq!(emoji.get_by_emoticon(":Z").unwrap(), "😒");
+        assert_eq!(emoji.get_by_emoticon(",:-[").unwrap(), "😓");
+        assert_eq!(emoji.get_by_emoticon("smile"), None);
+    }
+
+    #[test]
+    fn test_by_name() {
+        let emoji = Emojicon::new();
+        assert_eq!(emoji.get_by_name("happy").unwrap().collect::<Vec<_>>(), ["😀", "😃", "😄", "😆"]);
+        assert!(!emoji.get_by_name("none").is_some());
     }
 }
